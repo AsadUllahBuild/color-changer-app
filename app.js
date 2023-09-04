d05a6bb032de6e7b5da1a135bb7aeb15c0831c57
AsadUllahBuild <asadbuild97@gmail.com>
2023-09-04 07:34:57 +0500

    color-changer-app

---
+++ app.js
@@ -0,0 +1,56 @@
+const display = document.getElementById('main'); 
+const color = ["blue", "Red", "black", "orange", "yellow"];
+const body = document.querySelector('body'); 
+
+display.innerHTML += `<li> <button onClick="blue()" class="blue">${color.slice(0, 1)}</button> </li>`;
+display.innerHTML += `<li> <button onClick="red()" class="red">${color.slice(1, 2)}</button> </li>`;
+display.innerHTML += `<li> <button onClick="black()" class="black">${color.slice(2, 3)}</button> </li>`;
+display.innerHTML += `<li> <button onClick="orange()" class="orange">${color.slice(3, 4)}</button> </li>`;
+display.innerHTML += `<li> <button onClick="yellow()" class="yellow">${color.slice(4, 5)}</button> </li>`;
+
+
+function blue(){
+    switch (body) {
+        case body.style.backgroundColor == "white":
+
+            break;
+        case body.style.backgroundColor = "blue":
+            break;
+    }
+}
+function red(){
+    switch (body) {
+        case body.style.backgroundColor == "blue":
+
+            break;
+        case body.style.backgroundColor = "red":
+            break;
+    }
+}
+function black(){
+    switch (body) {
+        case body.style.backgroundColor == "red":
+
+            break;
+        case body.style.backgroundColor = "black":
+            break;
+    }
+}
+function orange(){
+    switch (body) {
+        case body.style.backgroundColor == "black":
+
+            break;
+        case body.style.backgroundColor = "orange":
+            break;
+    }
+}
+function yellow(){
+    switch (body) {
+        case body.style.backgroundColor == "orange":
+
+            break;
+        case body.style.backgroundColor = "yellow":
+            break;
+    }
+}
