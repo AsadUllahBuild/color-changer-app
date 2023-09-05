146e439118f8e7c1d8cfe69718d4e2f80822e35b
AsadUllahBuild <asadbuild97@gmail.com>
2023-09-05 22:46:17 +0500

    color changer app

--- app.js
+++ app.js
@@ -1,57 +1,18 @@
-const display = document.getElementById('main'); 
+const div = document.querySelector('div'); 
 const color = ["blue", "Red", "black", "orange", "yellow"];
 const body = document.querySelector('body'); 
-const data = document.getElementById("colors")
+// const para = document.querySelector('p');
 
-data.innerHTML += `<li> <button onClick="blue()" class="blue">${color.slice(0, 1)}</button> </li>`;
-data.innerHTML += `<li> <button onClick="red()" class="red">${color.slice(1, 2)}</button> </li>`;
-data.innerHTML += `<li> <button onClick="black()" class="black">${color.slice(2, 3)}</button> </li>`;
-data.innerHTML += `<li> <button onClick="orange()" class="orange">${color.slice(3, 4)}</button> </li>`;
-data.innerHTML += `<li> <button onClick="yellow()" class="yellow">${color.slice(4, 5)}</button> </li>`;
+// const data = document.getElementById("colors")
 
-
-function blue(){
-    switch (body) {
-        case body.style.backgroundColor == "white":
-
-            break;
-        case body.style.backgroundColor = "blue":
-            break;
-    }
+for (let i = 0; i < color.length; i++) {
+    div.innerHTML += `<button onclick='changeColor(${i})'>${color[i]}</button>`
+    
 }
-function red(){
-    switch (body) {
-        case body.style.backgroundColor == "blue":
 
-            break;
-        case body.style.backgroundColor = "red":
-            break;
-    }
-}
-function black(){
-    switch (body) {
-        case body.style.backgroundColor == "red":
 
-            break;
-        case body.style.backgroundColor = "black":
-            break;
-    }
-}
-function orange(){
-    switch (body) {
-        case body.style.backgroundColor == "black":
-
-            break;
-        case body.style.backgroundColor = "orange":
-            break;
-    }
-}
-function yellow(){
-    switch (body) {
-        case body.style.backgroundColor == "orange":
-
-            break;
-        case body.style.backgroundColor = "yellow":
-            break;
-    }
+function changeColor(i){
+    console.log(i);
+    body.style.backgroundColor = color[i];
+//    para.innerHTML += `${color[i]}`
 }
